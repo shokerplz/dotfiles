eb6e11aa482683d37cba3f7c19ecd5886050fe67
Ivan Kovalev <ivan@ikovalev.nl>
2025-04-18 19:54:11 +0200

    Update services

--- machines/media-server/packages.nix
+++ machines/media-server/packages.nix
@@ -2,7 +2,6 @@
 {
   environment.systemPackages = with pkgs; [
     zfs
-    linuxKernel.packages.linux_zen.zfs_2_3
     git
   ];
 }
--- services/versions.nix
+++ services/versions.nix
@@ -1,12 +1,12 @@
 {
-  jellyseerr = "2.5.0";
-  radarr = "5.17.2";
-  sonarr = "4.0.12";
+  jellyseerr = "2.5.2";
+  radarr = "5.21.1";
+  sonarr = "4.0.14";
   bazarr = "1.5.1";
-  prowlarr = "1.29.2";
-  jellyfin = "10.10.6";
-  qbittorrent = "5.0.3";
-  nzbget = "24.5.20250114";
-  nextcloud = "31.0.1";
+  prowlarr = "1.33.3";
+  jellyfin = "10.10.7";
+  qbittorrent = "5.0.5";
+  nzbget = "24.8";
+  nextcloud = "31.0.2";
   loki = "3.2.2";
 }
