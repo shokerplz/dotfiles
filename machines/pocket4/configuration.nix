14f20174f4edf12bbb9b3835bbd64b91ee1818ff
Ivan Kovalev <ivan@ikovalev.nl>
2025-04-19 00:38:25 +0200

    try to use proper package for fprintd

--- machines/pocket4/configuration.nix
+++ machines/pocket4/configuration.nix
@@ -5,7 +5,7 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ./packages.nix
-    ./libfprint-tod-pocket4.nix
+    ./fingerprint-scanner.nix
   ];
 
   # Bootloader.
--- machines/pocket4/fingerprint-scanner.nix
+++ machines/pocket4/fingerprint-scanner.nix
@@ -2,6 +2,11 @@
 
 let
   libfprint-focaltech = pkgs.callPackage ../../packages/libfprint-tod-pocket4.nix {};
+  pkgs = import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/21808d22b1cda1898b71cf1a1beb524a97add2c4.tar.gz";
+  }) {};
+
+  fprintd = pkgs.fprintd;
 in {
 
   # Enable fprintd 
@@ -9,6 +14,7 @@ in {
     enable = true;
     package = pkgs.fprintd.override {
       libfprint = libfprint-focaltech;
+			fprintd = fprintd;
     };
   };
 }
