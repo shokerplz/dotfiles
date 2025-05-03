9c4980d150a007e356aaae69779f78b06fc02a82
Ivan Kovalev <ivan4kovalev@gmail.com>
2025-05-03 14:58:58 +0200

    install smb share

---
+++ services/samba.nix
@@ -0,0 +1,32 @@
+services.samba = {
+  enable = true;
+  securityType = "user";
+  openFirewall = true;
+  settings = {
+    global = {
+      "workgroup" = "WORKGROUP";
+      "server string" = "mysmb";
+      "netbios name" = "mysmb";
+      "security" = "user";
+      "hosts allow" = "10. 127.0.0.1 localhost";
+      "hosts deny" = "0.0.0.0/0";
+      "guest account" = "nobody";
+      "map to guest" = "bad user";
+    };
+    "private" = {
+      "path" = "/mnt/ssd/smb";
+      "browseable" = "yes";
+      "read only" = "no";
+      "guest ok" = "no";
+      "create mask" = "0644";
+      "directory mask" = "0755";
+      "force user" = "ikovalev";
+      "force group" = "ikovalev";
+    };
+  };
+};
+
+services.samba-wsdd = {
+  enable = true;
+  openFirewall = true;
+};
