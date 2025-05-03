bb3f8622fe4dda2d9cbcf4aca0ed945ba5cbbce5
Ivan Kovalev <ivan4kovalev@gmail.com>
2025-05-03 17:31:54 +0200

    try to fix sound on main-pc

--- common/gui.nix
+++ common/gui.nix
@@ -17,13 +17,48 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber.configPackages = [
+      (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/alsa.conf" ''
+        monitor.alsa.rules = [
+          {
+            matches = [
+              {
+                device.name = "~alsa_card.*"
+              }
+            ]
+            actions = {
+              update-props = {
+                # Device settings
+                api.alsa.use-acp = true
+              }
+            }
+          }
+          {
+            matches = [
+              {
+                node.name = "~alsa_input.pci.*"
+              }
+              {
+                node.name = "~alsa_output.pci.*"
+              }
+            ]
+            actions = {
+            # Node settings
+              update-props = {
+                session.suspend-timeout-seconds = 0
+              }
+            }
+          }
+        ]
+      '')
+    ];
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
