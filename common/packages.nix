021e0e065b9cdccad8a92fc846e932614b6916b8
Ivan Kovalev <ivan4kovalev@gmail.com>
2025-05-30 23:54:39 +0200

    fix orca-slicer blank screen

--- common/packages.nix
+++ common/packages.nix
@@ -19,5 +19,6 @@
     jq
     btop
     dmidecode
+    file
   ];
 }
--- machines/main-pc/overlays.nix
+++ machines/main-pc/overlays.nix
@@ -4,10 +4,25 @@
   nixpkgs.overlays = with pkgs; [
     (final: prev: {
       # Overlay for orca-slicer. Without this package would fail to build
-      orca-slicer = prev.orca-slicer.overrideAttrs (oldAttrs: {
-        cmakeFlags = oldAttrs.cmakeFlags ++ [
-          (lib.cmakeFeature "CUDA_TOOLKIT_ROOT_DIR" "${prev.cudaPackages.cudatoolkit}")
+      orca-slicer = prev.orca-slicer.overrideAttrs (old: rec {
+        ## 1 – your original CUDA flag
+        cmakeFlags = (old.cmakeFlags or [ ]) ++ [
+          (prev.lib.cmakeFeature "CUDA_TOOLKIT_ROOT_DIR" "${prev.cudaPackages.cudatoolkit}")
         ];
+
+        ## 2 – we need makeWrapper for the wrapper script
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.makeWrapper ];
+
+        ## 3 – wrap the real binary with env-var glue
+        postFixup =
+          (old.postFixup or "")
+          + ''
+            wrapProgram "$out/bin/orca-slicer" \
+              --set __EGL_VENDOR_LIBRARY_FILENAMES "${prev.pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json" \
+              --set WEBKIT_DISABLE_DMABUF_RENDERER "1" \
+              --set WEBKIT_DISABLE_COMPOSITING_MODE "1" \
+              --set WEBKIT_FORCE_COMPOSITING_MODE "1" 
+          '';
       });
     })
   ];
