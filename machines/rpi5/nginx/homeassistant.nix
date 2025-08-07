704e325741a688f4864e1d0acfe3acf6047dcc8a
Ivan Kovalev <ivan4kovalev@gmail.com>
2025-08-07 22:10:55 +0200

    try to fix home assistant

--- machines/rpi5/nginx/homeassistant.nix
+++ machines/rpi5/nginx/homeassistant.nix
@@ -11,11 +11,11 @@
     acmeRoot = null;
     listenAddresses = ["10.0.1.20"];
     extraConfig = ''
-      proxy_headers_hash_max_size 4096;
-      proxy_headers_hash_bucket_size  128;
       proxy_set_header Host $host;
       proxy_set_header X-Real-IP $remote_addr;
       proxy_redirect http:// https://;
+      proxy_set_header    Upgrade     $http_upgrade;
+      proxy_set_header    Connection  "upgrade";
     '';
     locations."/" = {
       proxyPass = "http://homeassistant.home:8123";
