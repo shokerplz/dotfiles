6e7e19f36d8f52d8b7307673fad533e74b95dd7d
Ivan Kovalev <ivan4kovalev@gmail.com>
2025-12-05 20:24:10 +0100

    chore: switch to upstream cloudflare-ddns service

--- services/cloudflare-ddns.nix
+++ services/cloudflare-ddns.nix
@@ -1,10 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ../modules/cloudflare-ddns.nix
-  ];
-
   # Cloudflare DDNS service to announce my public ip
   config.services.cloudflare-ddns = {
     enable = true;
@@ -12,10 +8,4 @@
     domains = [ "vpn.ikovalev.nl" ];
     proxied = "false";
   };
-
-  config.nixpkgs.overlays = [
-    (final: prev: {
-      cloudflare-ddns = final.callPackage ../packages/cloudflare-ddns.nix { };
-    })
-  ];
 }
