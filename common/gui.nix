65b2fc1bb1374315be3d004074dce1cfd5ede2d2
Ivan Kovalev <ivan4kovalev@gmail.com>
2025-12-05 20:30:50 +0100

    fix: update deprecated options

--- common/gui.nix
+++ common/gui.nix
@@ -8,8 +8,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Disable gnome remote desktop (it doesn't work with pulseaudio)
   services.gnome.gnome-remote-desktop.enable = false;
