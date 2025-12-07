d1ff932795cb190dea1ba73e7bebab53bcb7e04a
Ivan Kovalev <ivan4kovalev@gmail.com>
2025-12-07 22:41:52 +0100

    Add git secret to builder

--- machines/media-server/nix-cache.nix
+++ machines/media-server/nix-cache.nix
@@ -26,6 +26,25 @@
     };
   };
 
+  systemd.services.nix-local-cache-server = {
+    environment.GIT_SSH_COMMAND = "ssh -i ${config.sops.secrets.git_key.path} -o IdentitiesOnly=yes";
+  };
+
+  programs.ssh.knownHosts = {
+    "github.com" = {
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+      extraHostNames = ["github"]; # Optional aliases
+    };
+
+    "gitlab.com" = {
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
+    };
+
+    "git.ikovalev.nl" = {
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGNA+lEEaFORzkxVwC0FpwJpkGVUA32nwDXkkVZ7kHnr";
+    };
+  };
+
   services.nix-local-cache-server = {
     enable = true;
     package = nix-local-cache.packages.${pkgs.system}.server;
