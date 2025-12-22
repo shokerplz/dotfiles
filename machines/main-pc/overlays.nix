6210b1c33065c0c971ffcf01519100db310e8aaf
Ivan Kovalev <ivan4kovalev@gmail.com>
2025-12-22 15:06:28 +0100

    Improve orca overlay

--- machines/main-pc/overlays.nix
+++ machines/main-pc/overlays.nix
@@ -23,6 +23,10 @@
             wrapProgram "$out/bin/orca-slicer" \
               --set __EGL_VENDOR_LIBRARY_FILENAMES "${prev.pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json" \
               --set WEBKIT_DISABLE_COMPOSITING_MODE "1" \
+              --set WEBKIT_DISABLE_DMABUF_RENDERER "1" \
+              --set MESA_LOADER_DRIVER_OVERRIDE "zink" \
+              --set GALLIUM_DRIVER "zink" \
+              --set __GLX_VENDOR_LIBRARY_NAME "mesa"
           '';
       });
     })
