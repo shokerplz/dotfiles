366134e7f812f8bf5403363c08f844a19e8e3e32
Ivan Kovalev <ivan4kovalev@gmail.com>
2025-12-22 23:58:05 +0100

    that will surely help

--- machines/main-pc/overlays.nix
+++ machines/main-pc/overlays.nix
@@ -22,7 +22,6 @@
           + ''
             wrapProgram "$out/bin/orca-slicer" \
               --set __EGL_VENDOR_LIBRARY_FILENAMES "${prev.pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json" \
-              --set WEBKIT_DISABLE_COMPOSITING_MODE "1" \
               --set WEBKIT_DISABLE_DMABUF_RENDERER "1" \
               --set MESA_LOADER_DRIVER_OVERRIDE "zink" \
               --set GALLIUM_DRIVER "zink" \
