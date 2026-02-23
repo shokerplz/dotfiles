2f9384131d0aab5ccc920f950a91004ea561e9d9
Ivan Kovalev <ivan4kovalev@gmail.com>
2026-02-23 20:23:02 +0100

    Fix n8n

--- services/n8n.nix
+++ services/n8n.nix
@@ -1,7 +1,4 @@
-{ ... }:
-
-{
-
+{pkgs, ...}: {
   services.n8n = {
     enable = true;
     environment = {
@@ -10,6 +7,7 @@
       WEBHOOK_URL = "https://n8n.ikovalev.nl/";
     };
   };
+  systemd.services.n8n.path = [pkgs.python3 pkgs.bun pkgs.nodejs];
 
   networking.firewall.extraCommands = ''
     iptables -A nixos-fw -p tcp --dport 5678 -s 10.0.0.0/16 -j nixos-fw-accept
@@ -17,5 +15,4 @@
   networking.firewall.extraStopCommands = ''
     iptables -D nixos-fw -p tcp --dport 5678 -s 10.0.0.0/16 -j nixos-fw-accept || true
   '';
-
 }
