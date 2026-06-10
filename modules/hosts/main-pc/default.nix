09714407cf8ba35464fddd0956474587d0238d24
Ivan Kovalev <ivan4kovalev@gmail.com>
2026-06-11 01:57:53 +0200

    Add MT7927 wireless support

--- modules/hosts/main-pc/default.nix
+++ modules/hosts/main-pc/default.nix
@@ -12,6 +12,7 @@
     inputs.nixpkgs-current.lib.nixosSystem {
       modules = [
         self.nixosModules.mainPCConfiguration
+        inputs.mt7927.nixosModules.default
       ];
       specialArgs = {
         inherit pkgs-unstable;
